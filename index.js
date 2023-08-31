2e5222efd7d4b09b6801eea5e0d5cbfd9794beba
Ario Nugraha Ramadhanu <arionugraha5@gmail.com>
2023-09-01 02:30:02 +0700

    added port listen

--- index.js
+++ index.js
@@ -41,3 +41,8 @@ app.get("/api/:date?", function(req, res) {
 
   res.json(jsonReturnFactory(date));
 });
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
